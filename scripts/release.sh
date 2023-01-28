b8cd10fbdab402db1b7f0b406895fb1a0d529f4f
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-01-28 13:15:18 -0800

    Reformatted/cleaned 'release.sh'

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ VERSION=$1
 REPO=docker/docker-py
 GITHUB_REPO=git@github.com:$REPO
 
-if [ -z $VERSION ]; then
+if [ -z "$VERSION" ]; then
     echo "Usage: $0 VERSION [upload]"
     exit 1
 fi
@@ -17,20 +17,19 @@ git clean -x -d -i
 test -z "$(git clean -x -d -n)" || exit 1
 
 echo "##> Tagging the release as $VERSION"
-git tag $VERSION
-if [[ $? != 0 ]]; then
+if ! git tag "$VERSION"; then
     head_commit=$(git show --pretty=format:%H HEAD)
-    tag_commit=$(git show --pretty=format:%H $VERSION)
-    if [[ $head_commit != $tag_commit ]]; then
+    tag_commit=$(git show --pretty=format:%H "$VERSION")
+    if [[ "$head_commit" != "$tag_commit" ]]; then
         echo "ERROR: tag already exists, but isn't the current HEAD"
         exit 1
     fi
 fi
-if  [[ $2 == 'upload' ]]; then
-    echo "##> Pushing tag to github"
-    git push $GITHUB_REPO $VERSION || exit 1
-fi
 
+if [[ $2 == 'upload' ]]; then
+    echo "##> Pushing tag to github"
+    git push $GITHUB_REPO "$VERSION" || exit 1
+fi
 
 echo "##> sdist & wheel"
 python setup.py sdist bdist_wheel
