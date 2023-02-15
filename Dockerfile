16f14497c457f1444bcc8e32d19f3ca3fe1472bf
Joel Van Eenwyk <Joel.Van.Eenwyk@microsoft.com>
2023-02-14 16:40:25 -0800

    Improved Docker config

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-# syntax=docker/dockerfile:1
+# syntax=docker/dockerfile:2
 
-ARG PYTHON_VERSION=2.7
-
+ARG PYTHON_VERSION=3.10
 FROM python:${PYTHON_VERSION}
 
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && apt-get install -y bash
+
 # Add SSH keys and set permissions
-COPY tests/ssh-keys /root/.ssh
+COPY ./tests/ssh-keys /root/.ssh
 RUN chmod -R 600 /root/.ssh
+
+ENV HOME="/root"
+ENV PATH="${PATH}:$HOME/.local/bin"
 
 WORKDIR /src
 
@@ -18,6 +23,33 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY requirements-test.txt ./requirements-test.txt
 RUN pip install --no-cache-dir -r requirements-test.txt
 
+ARG DEV_MODE=0
+ENV PYENV_ROOT="$HOME/.pyenv"
+ENV PATH="${PATH}:$PYENV_ROOT/bin"
+ENV PY_VERSIONS="3.10.9 3.9.16 3.11.1 3.8.16 3.7.16 2.7.18 pypy3.9-7.3.11"
+
+RUN curl https://pyenv.run | bash \
+    && echo 'export PYENV_ROOT="$HOME/.pyenv"' >>"$HOME/.bashrc" \
+    && echo 'export PATH="$PYENV_ROOT/bin:$PATH"'>>"$HOME/.bashrc" \
+    && echo 'eval "$(pyenv init -)"'>>"$HOME/.bashrc" \
+    && eval "$(pyenv init -)"
+
+SHELL ["/bin/bash", "-c"]
+RUN if [ ! $DEV_MODE = 0 ]; then \
+      versions=($(echo "$PY_VERSIONS" | tr ' ' '\n')); \
+      pyenv install "${versions[@]}"; \
+      pyenv global "${versions[@]}"; \
+      pyenv local "${versions[@]}"; \
+      pyenv shell "${versions[@]}"; \
+    fi
+
 COPY . ./
 ARG SETUPTOOLS_SCM_PRETEND_VERSION_DOCKER
-RUN pip install --no-cache-dir .
+# RUN pip install --no-cache-dir .
+RUN pip install tox
+
+RUN mkdir /workspace
+WORKDIR /workspace
+
+ENTRYPOINT ["/bin/bash", "-c"]
+CMD ["bash"]
--- setup.sh
+++ setup.sh
@@ -1,79 +1,85 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+#!/usr/bin/env bash
 
 _sudo() {
-    if [ -x "$(command -v apt-get)" ]; then
+    if [ -n "$(command -v sudo)" ]; then
         sudo "$@"
     else
         "$@"
     fi
 }
 
-_sudo chown vscode .tox || true
+main() {
+    set -eou
 
-if [ -x "$(command -v apt-get)" ]; then
-    if _sudo apt-get update; then
-        _sudo apt-get install -y --no-install-recommends rsync
-    fi
-fi
+    SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-if [ -x "$(command -v pyenv)" ]; then
-    echo "Found 'pyenv' installation: $(command -v pyenv)"
-else
-    # curl https://pyenv.run | bash
-    if [ ! -e "$HOME/.pyenv" ]; then
-        git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
-        (
-            cd "$HOME/.pyenv" || true
-            src/configure
-            make -C src
-        )
+    _sudo chown vscode .tox || true
+
+    if [ -x "$(command -v apt-get)" ]; then
+        if _sudo apt-get update; then
+            _sudo apt-get install -y --no-install-recommends rsync
+        fi
     fi
 
-    # shellcheck disable=SC2016
-    {
-        echo 'export PYENV_ROOT="$HOME/.pyenv"'
-        echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
-        echo 'eval "$(pyenv init -)"'
-    } >>"$HOME/.bashrc"
+    if [ -x "$(command -v pyenv)" ]; then
+        echo "Found 'pyenv' installation: $(command -v pyenv)"
+    else
+        # curl https://pyenv.run | bash
+        if [ ! -e "$HOME/.pyenv" ]; then
+            git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
+            (
+                cd "$HOME/.pyenv" || true
+                src/configure
+                make -C src
+            )
+        fi
 
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    echo "Initialized 'pyenv' environment: '${PYENV_ROOT}'"
-fi
+        # shellcheck disable=SC2016
+        {
+            echo 'export PYENV_ROOT="$HOME/.pyenv"'
+            echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
+            echo 'eval "$(pyenv init -)"'
+        } >>"$HOME/.bashrc"
 
-if [ -x "$(command -v pyenv)" ]; then
-    versions=("3.10.9" "3.9.16" "3.11.1" "3.8.16" "3.7.16" "2.7.18" "pypy3.9-7.3.11")
-    pyenv install --skip-existing "${versions[@]}"
-
-    if [ ! -e "$SCRIPT_DIR/.python-version" ]; then
-        pyenv global "${versions[@]}"
-        pyenv local "${versions[@]}"
-        pyenv shell "${versions[@]}"
+        export PYENV_ROOT="$HOME/.pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
+        echo "Initialized 'pyenv' environment: '${PYENV_ROOT}'"
     fi
-fi
 
-if [ ! -x "$(command -v poetry)" ]; then
-    curl -sSL https://install.python-poetry.org | python3 -
-fi
+    if [ -x "$(command -v pyenv)" ]; then
+        versions=("3.10.9" "3.9.16" "3.11.1" "3.8.16" "3.7.16" "2.7.18" "pypy3.9-7.3.11")
+        pyenv install --skip-existing "${versions[@]}"
 
-if [ -x "$(command -v poetry)" ]; then
-    poetry install --only main
-fi
+        if [ ! -e "$SCRIPT_DIR/.python-version" ]; then
+            pyenv global "${versions[@]}"
+            pyenv local "${versions[@]}"
+            pyenv shell "${versions[@]}"
+        fi
+    fi
 
-if [ -x "$(command -v python)" ]; then
-    python -m pip install -U wheel pip setuptools
-    python -m pip install \
-        --user --no-warn-script-location \
-        -r "$SCRIPT_DIR/requirements-dev.txt"
-fi
+    if [ ! -x "$(command -v poetry)" ]; then
+        curl -sSL https://install.python-poetry.org | python3 -
+    fi
 
-if [ -e "/local" ]; then
-    rsync \
-        --exclude '.tox' --exclude '__pycache__' --exclude '.mypy_cache' --exclude '.pytest_cache' --exclude '*.pyc' \
-        --exclude 'docker.egg-info' --exclude '.coverage.*' \
-        -v -a \
-        /workspace/ /local/
-fi
+    if [ -x "$(command -v poetry)" ]; then
+        poetry install --only main
+    fi
+
+    if [ -x "$(command -v python)" ]; then
+        python -m pip install -U wheel pip setuptools tox
+        python -m pip install \
+            --user \
+            -r "$SCRIPT_DIR/requirements-dev.txt"
+    fi
+
+    if [ -e "/local" ]; then
+        rsync \
+            --exclude '.tox' --exclude '__pycache__' --exclude '.mypy_cache' --exclude '.pytest_cache' --exclude '*.pyc' \
+            --exclude 'docker.egg-info' --exclude '.coverage.*' \
+            -v -a \
+            /workspace/ /local/
+    fi
+}
+
+main "$@"
